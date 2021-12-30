daeade271c80dd20e5f5b85ba4cdc5f2d817ea08
Cody Curry <cocurry1@gmail.com>
2021-12-30 09:14:24 -0500

    Add SameStarChar

---
+++ java/src/main/com/codingbat/string2/SameStarChar.java
@@ -0,0 +1,11 @@
+package main.com.codingbat.string2;
+
+public class SameStarChar {
+  public static boolean sameStarChar(String str) {
+    for (int i=1; i < str.length()-1; i++) {
+      if (str.charAt(i) == '*' && str.charAt(i-1) != str.charAt(i+1)) return false;
+    }
+    return true;
+  }
+  
+}
